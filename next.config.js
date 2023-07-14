b0348dd3f8684f73e7b2f7775aedffec1a347fb0
Weiran Li <weiran@second.dev>
2023-07-14 11:21:00 -0700

    manual fixes

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   env: {
     PUBLIC_URL: ''
   },
-  experimental: {
-    craCompat: true,
-  },
+  // experimental: {
+  //   craCompat: true,
+  // },
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   images: {
